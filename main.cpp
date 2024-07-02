0385cf89e2164187093d0d042b557a460fb44ede
xp-squared <zaneimportant@gmail.com>
2024-07-02 16:35:06 -0400

    Starting project

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+void intro() {
+    cout << "\n***********************************************************************\nWelcome to Eternal Labyrinth, a text based game created by Zane Christe\n***********************************************************************\n\n";
+}
+
+int main() {
+    intro();
+
+    return 0;
+}
